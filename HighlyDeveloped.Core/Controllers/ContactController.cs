9a7379e4100f6e8f4bb32b591b09d578f5c156ef
Alliah <alliahczarielle17@gmail.com>
2021-07-01 16:22:10 +1200

    Add project files.

---
+++ HighlyDeveloped.Core/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using HighlyDeveloped.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace HighlyDeveloped.Core.Controllers
+{/// <summary>
+/// This is the stage where we create the controller after creating and 
+/// editing the macro files, make sure to inherit from the SurfaceController
+/// this class if for all the operations regarding a contact form
+/// </summary>
+    public class ContactController : SurfaceController
+    {
+        //Create the action result
+
+        public ActionResult RenderContactForm() 
+        {
+            //Then return the partial view for contact form that we need to create
+            //and pass a view model that we need to create
+
+            var vm = new ContactFormViewModel();
+            return PartialView("~/Views/Partials/Contact Form.cshtml", vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleContactForm(ContactFormViewModel vm)
+        {
+            return null;
+        }
+        
+    }
+}
